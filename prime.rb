82ef905b816efd7de503e72167259044cbb917ab
Elbin Keeppanasseril <elbincodes@gmail.com>
2017-08-20 23:53:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,30 @@
 # Add  code here!
+def prime?(num)
+  if num <=1
+    return false
+  end
+    myArr = (2...num).to_a
+
+  myArr.each do |x|
+    if num%x == 0
+      return false
+    end
+
+  end
+  return true
+end
+
+
+#  why does the below not work?
+# def prime?(num)
+#   myArr = (2...num).to_a
+#
+#   myArr.each do |x|
+#     if num%x == 0
+#       return false
+#     else
+#       return true
+#     end
+#
+#   end
+# end
